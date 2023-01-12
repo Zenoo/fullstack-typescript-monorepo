4fc29c4349d12d3b1c5596948e5896bc5d5f17e4
Zenoo <perso@franckdemoute.fr>
2023-01-12 15:02:30 +0100

    fix: i18n init props

--- server/src/i18n.ts
+++ server/src/i18n.ts
@@ -2,13 +2,13 @@ import i18next from 'i18next';
 import Backend from 'i18next-fs-backend';
 
 i18next.use(Backend).init({
-  lng: 'fr',
+  lng: 'en',
   backend: {
     /* translation file path */
-    loadPath: '/../i18n/{{lng}}.json',
+    loadPath: 'i18n/{{lng}}.json',
   },
   fallbackLng: 'en',
-  debug: false,
+  debug: true,
   returnNull: false,
 }).catch((err) => {
   console.error('Error loading language', err);
